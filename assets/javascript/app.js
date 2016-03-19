899f3360f923020dcd92a1dec9818814355211a6
Robert Black <rbla4655@gmail.com>
2016-03-19 07:17:37 -0400

    layout tests

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,34 @@
 
-
+jQuery(document).ready(function(){
     var id = "";
     var url = "";
     $("#submit").click(function(){
-        if($("#artistInput").val() != ""){
-            findArtist($("#artistInput").val());
-            getTourDates($("#artistInput").val());
-        } else if($("#venueInput").val() != ""){
+        if($(this).data("target") == "artist" && $("#artistInput").val() != ""){
+            debugger;
+            searchByArtist($("#artistInput").val());
+        } else if($(this).data("target") == "venue" && $("#venueInput").val() != ""){
             findVenue($("#venueInput").val());
         }
+        return false;
     });
+    function searchByArtist(band){
+        findArtist(band);
+        getTourDates(band);
+        $("#resultDiv").attr("style", "display: initial");
+        setTimeout(function(){
+            $(".inputBox").val("")
+        }, 1000)
+    };
     $("#artistInput").on("focus", function(){
         var keyPress = 0;
         $(window).keyup(function(event) {
             if(event.keyCode == 13){
                 if(keyPress == 0){
-                    findArtist($("#artistInput").val());
-                    getTourDates($("#artistInput").val());
+                    searchByArtist($("#artistInput").val());
                 } else {
-                    findArtist($("#autoArt"+keyPress).data("artist"));
-                    getTourDates($("#autoArt"+keyPress).data("artist"));
-                }
-                $("#artistInput").val("");
-                $("#artistDropDown").attr("style", "display: none")
-                $("#tableBody").focus();
-                $("#tableBody").select();
+                    searchByArtist($("#autoArt"+keyPress).data("artist"));
+                };
+                $("#artistDropDown").attr("style", "display: none");
             } else if(event.keyCode == 40){
                 keyPress++;
                 $(".dropDownItem").attr("style", "");
@@ -64,11 +68,19 @@
         $("#artistDropDown").attr("style", "display: none");
     });
     function findArtist(artist){
-        $("#searchDivTitle").text("Recommended to "+artist+" fans:");
+        $("#searchDivTitle").html("<h3>Recommended for "+artist+" fans!</h3>");
         url = "https://api.spotify.com/v1/search?q="+artist+"&type=artist";
         $.ajax({url: url, method: "GET"})
         .done(function(response) {
+            console.log(response);
             id = response.artists.items[0].id;
+            if(id != null){
+                $("#bandInfo").attr("style", "display: initial");
+                $("#searchContainer").attr("style", "display: none");
+                $("#bandImage").html("<img class='mainBandImage' style='margin-bottom: 40px' src='"+response.artists.items[0].images[0].url+"''>");
+                $("#bandHeader").html("<h2 class='mainBandHeader'>"+response.artists.items[0].name+"</h2>");
+                $("#bandDiscription").html("<h3>Band Info</h3><br><button class='btn btn-warning backToSearch'>Search New Artist</button>")
+            }
             url = "https://api.spotify.com/v1/artists/"+id+"/related-artists"
             $.ajax({url: url, method: "GET"})
             .done(function(response) {
@@ -119,7 +131,13 @@
                 console.log(data);
                 $("#tableBody").html("");
                 for(var i=0; i<10; i++){
-                    $("#tableBody").append("<tr><td>"+data[i].datetime.substring(5, 7)+"/"+data[i].datetime.substring(8, 10)+"/"+data[i].datetime.substring(2, 4)+"</td><td>"+data[i].venue.name+"</td><td>"+data[i].venue.region+"</td><td>"+data[i].venue.city+"</td></tr>");
+                    var ticketSpot;
+                    if(data[i].ticket_status == "available"){
+                        ticketSpot = "<button class='btn btn-success buyTixBtn' data-url='"+data[i].ticket_url+"'>Buy</button>";
+                    } else {
+                        ticketSpot = "Sold Out";
+                    }
+                    $("#tableBody").append("<tr><td>"+data[i].datetime.substring(5, 7)+"/"+data[i].datetime.substring(8, 10)+"/"+data[i].datetime.substring(2, 4)+"</td><td>"+data[i].venue.name+"</td><td>"+data[i].venue.region+"</td><td>"+data[i].venue.city+"</td><td>"+ticketSpot+"</td></tr>");
                 }
             },
             error: function(errorObj){
@@ -128,17 +146,37 @@
         });
     }
     $("body").on("click", ".relatedSearchBtn", function(){
-        findArtist($(this).data("artist"));
-        getTourDates($(this).data("artist"));
+        searchByArtist($(this).data("artist"));
     }); 
     $(".relatedTab").click(function(){
         $(".relatedTab").removeClass("active");
         $(this).addClass("active");
         $(".tabDisplay").attr("style", "display: none");
         var div = $(this).data("div");
-        $("#"+div).attr("style", "display: initial")
-    })
+        $("#"+div).attr("style", "display: initial");
+    });
+    $(".searchTab").click(function(){
+        var target = $(this).data("target")
+        if($(this).hasClass("active")) return;
+        $(".searchTab").removeClass("active");
+        $(this).addClass("active");
+        $(".inputBox").attr("style", "display: none");
+        $(".inputLabel").attr("style", "display: none");
+        $("#submit").data("target", target);
+        $("."+target+"SI").attr("style", "display: initial");
+        $(".inputBox").attr("required", "");
+        $("#"+target+"Input").attr("required", "required");
+    });
+    function findVenue(){
 
-
+    };
+    $("body").on("click", ".backToSearch", function(){
+        $("#bandInfo").attr("style", "display: none");
+        $("#searchContainer").attr("style", "display: initial");
+    });
+    $("body").on("click", ".buyTixBtn", function(){
+        window.location.replace($(this).data("url"));
+    });
+});
 
      
