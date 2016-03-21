0b84577535ebbdae40d846171e344e954ddf7189
Kevo <kevo548@gmail.com>
2016-03-20 20:49:25 -0400

    added robs 3/20 work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,52 @@
 // rob's code
 
-
 jQuery(document).ready(function(){
     var id = "";
     var url = "";
+    $("#artistInput").focus();
+    $("#artistInput").select();
     $("#submit").click(function(){
         if($(this).data("target") == "artist" && $("#artistInput").val() != ""){
-            searchByArtist($("#artistInput").val());
+            searchByArtist($("#artistInput").val(), $("#startDateInput").val(), $("#endDateInput").val());
         } else if($(this).data("target") == "venue" && $("#venueInput").val() != ""){
             findVenue($("#venueInput").val());
+        } else {
+            return;
         }
+        console.log($("#startDateInput").val())
         return false;
+
     });
-    function searchByArtist(band){
+    function searchByArtist(band, start, end){
+        if(band == "") return;
+        $("#topTracks").html("");
+        $("#relatedShowBody").html("");
+
         findArtist(band);
-        getTourDates(band);
+        getTourDates(band, start, end);
         $("#resultDiv").attr("style", "display: initial");
         setTimeout(function(){
             $(".inputBox").val("")
-        }, 1000)
+        }, 1000);
     };
     $("#artistInput").on("focus", function(){
         var keyPress = 0;
         $(window).keyup(function(event) {
             if(event.keyCode == 13){
                 if(keyPress == 0){
-                    searchByArtist($("#artistInput").val());
+                    searchByArtist($("#artistInput").val(), $("#startDateInput").val(), $("#endDateInput").val());
                 } else {
-                    searchByArtist($("#autoArt"+keyPress).data("artist"));
+                    searchByArtist($("#autoArt"+keyPress).data("artist"), $("#startDateInput").val(), $("#endDateInput").val());
                 };
                 $("#artistDropDown").attr("style", "display: none");
             } else if(event.keyCode == 40){
                 keyPress++;
                 $(".dropDownItem").attr("style", "");
-                $("#autoArt"+keyPress).attr("style", "background-color: white");
+                $("#autoArt"+keyPress).attr("style", "background-color: white; color: purple");
             } else if(event.keyCode == 38){
                 keyPress--;
                 $(".dropDownItem").attr("style", "");
-                $("#autoArt"+keyPress).attr("style", "background-color: white");
+                $("#autoArt"+keyPress).attr("style", "background-color: white; color: purple");
             } else {
                 if($("#artistInput").val().length > 1){
                     keyPress = 0;
@@ -50,7 +59,7 @@ jQuery(document).ready(function(){
                             $("#artistDropDown").html("");
                             $("#artistInput").addClass("panel-header");
                             for(var i=0; i<5 && i<response.artists.items.length; i++){
-                               $("#artistDropDown").append("<div class='panel-footer dropDownItem' id='autoArt"+(i+1)+"' data-artist='"+response.artists.items[i].name+"'>"+response.artists.items[i].name+"</div>");
+                               $("#artistDropDown").append("<div class='panel-footer dropDownItem dropThis' id='autoArt"+(i+1)+"' data-artist='"+response.artists.items[i].name+"'>"+response.artists.items[i].name+"</div>");
                             };
                         } else {
                             $("#artistDropDown").html("");
@@ -66,7 +75,9 @@ jQuery(document).ready(function(){
     });
     $("#artistInput").on("focusout", function(){
         $("#artistInput").removeClass("panel-header");
-        $("#artistDropDown").attr("style", "display: none");
+        setTimeout(function(){
+            $("#artistDropDown").attr("style", "display: none");
+        }, 200); 
     });
     function findArtist(artist){
         $("#searchDivTitle").html("<h3>Recommended for "+artist+" fans!</h3>");
@@ -80,7 +91,10 @@ jQuery(document).ready(function(){
                 $("#searchContainer").attr("style", "display: none");
                 $("#bandImage").html("<img class='mainBandImage' style='margin-bottom: 40px' src='"+response.artists.items[0].images[0].url+"''>");
                 $("#bandHeader").html("<h2 class='mainBandHeader'>"+response.artists.items[0].name+"</h2>");
-                $("#bandDiscription").html("<h3>Band Info</h3><br><button class='btn btn-warning backToSearch'>Search New Artist</button>")
+                $("#bandGenreInfo").html(response.artists.items[0].genres[0]);
+                $("#spotifyConnect").data("url", response.artists.items[0].href);
+                $("#bandFollowers").html(response.artists.items[0].followers.total);
+                $("#bandPopularity").html(response.artists.items[0].popularity);
             }
             url = "https://api.spotify.com/v1/artists/"+id+"/related-artists"
             $.ajax({url: url, method: "GET"})
@@ -88,18 +102,18 @@ jQuery(document).ready(function(){
                 $("#spotArtists").html("");
                 $("#artistsBadge").text(response.artists.length)
                 for(var i=0; i<response.artists.length; i++){
-                    $("#spotArtists").append("<div class='spotRes relatedArtist'><div class='thumbnail related'><div class='artistImg'><img src='"+response.artists[i].images[0].url+"' class='img-responsive' style='max-height: 100px; max-width: 100px;'></div><div class='artistTitle'><span class='relTitle'>Artist: </span>"+response.artists[i].name+"<div class='relTitle'>Genre: "+response.artists[i].genres[0]+"</div><br><button data-artist='"+response.artists[i].name+"' class='relatedSearchBtn btn btn-primary'><span class='searchBtnText'>Search Shows </span><span class='glyphicon glyphicon-search' aria-hidden='true'></span></button></div></div><div>");
+                    $("#spotArtists").append("<div class='spotRes relatedArtist' data-artist='"+response.artists[i].name+"'><div class='thumbnail related'><div class='artistImg'><img src='"+response.artists[i].images[0].url+"' class='img-responsive' style='max-height: 100px; max-width: 100px;'></div><div class='artistTitle'><span class='relTitle'>Artist: </span>"+response.artists[i].name+"<div class='relTitle'>Genre: "+response.artists[i].genres[0]+"</div></div></div></div>");
                 }
             });
-            url = "https://api.spotify.com/v1/artists/"+id+"/top-tracks?country=US"
+            url = "https://api.spotify.com/v1/artists/"+id+"/top-tracks?country=US";
             $.ajax({url: url, method: "GET"})
             .done(function(ref) {
                 $("#topBadge").text(ref.tracks.length)
                 for(var j=0; j<ref.tracks.length; j++){
-                    $("#topTracks").append("<div class='topTrackDiv thumbnail'><span><h2 style='background-color: #337ab7; color: white'>#"+(j+1)+" "+ref.tracks[j].name+"</h2></span>Album: "+ref.tracks[j].album.name+"<br><audio controls volume='false'><source src='"+ref.tracks[j].preview_url+"''></audio></div>")
+                    $("#topTracks").append("<div class='topTrackDiv thumbnail blackBack'><span class='purpleBack'><h2 style='color: white; font-size: 24px'>#"+(j+1)+" "+ref.tracks[j].name+"</h2></span>Album: "+ref.tracks[j].album.name+"<br><audio controls volume='false'><source src='"+ref.tracks[j].preview_url+"''></audio></div>")
                 }
             });
-            url = "https://crossorigin.me/http://api.bandsintown.com/artists/"+artist+"/events/recommended?&location=New+York,NY&radius=150&app_id=muskick&api_version=2.0&format=json";
+            url = "https://crossorigin.me/http://api.bandsintown.com/artists/"+artist+"/events/recommended?&location=New+York,NY&radius=150&app_id=muskick3&api_version=2.0&format=json";
             $.ajax({
                 type: 'GET',
                 url: url,
@@ -121,8 +135,11 @@ jQuery(document).ready(function(){
         });
     };
 
-    function getTourDates(artist){
-        var bandTownurl = "https://crossorigin.me/https://api.bandsintown.com/artists/"+artist+"/events.json?callback=?&app_id=muskick"
+    function getTourDates(artist, start, end){
+        var date = "";
+        if(start != "" && end != "") date = "date="+start+","+end+"&";
+        if(start != "" && end == "") date = "date="+start+"&";
+        var bandTownurl = "https://crossorigin.me/https://api.bandsintown.com/artists/"+artist+"/events.json?"+date+"callback=?&app_id=muskick3";
         console.log(bandTownurl);
         $.ajax({
             type: 'GET',
@@ -131,23 +148,43 @@ jQuery(document).ready(function(){
             success: function(data){
                 console.log(data);
                 $("#tableBody").html("");
-                for(var i=0; i<data.length; i++){
+                var concertNum = data.length;
+                var pageNum = (concertNum - (concertNum%5))/5;
+                if(concertNum%5 != 0) pageNum++;
+                var page = 1;
+                for(var i=0; i<concertNum; i++){
                     var ticketSpot;
+                    console.log(page);
+                    if(i != 0 && i%5 == 0) page++;
                     if(data[i].ticket_status == "available"){
-                        ticketSpot = "<button class='btn btn-success buyTixBtn' data-url='"+data[i].ticket_url+"'>Buy</button>";
+                        ticketSpot = "<button class='btn btn-info buyTixBtn buttonText' data-url='"+data[i].ticket_url+"'>Buy</button>";
                     } else {
                         ticketSpot = "Sold Out";
                     }
-                    $("#tableBody").append("<tr><td>"+data[i].datetime.substring(5, 7)+"/"+data[i].datetime.substring(8, 10)+"/"+data[i].datetime.substring(2, 4)+"</td><td>"+data[i].venue.name+"</td><td>"+data[i].venue.region+"</td><td>"+data[i].venue.city+"</td><td>"+ticketSpot+"</td></tr>");
-                }
+                    debugger
+                    $("#tableBody").append("<tr class='pageNum"+page+" tourPage'><td>"+data[i].datetime.substring(5, 7)+"/"+data[i].datetime.substring(8, 10)+"/"+data[i].datetime.substring(2, 4)+"</td><td>"+data[i].venue.name+"</td><td>"+data[i].venue.region+"</td><td>"+data[i].venue.city+"</td><td>"+ticketSpot+"</td></tr>");
+                };
+                $("#pageDirectory").attr("style", "display: none");
+                $(".tourPage").attr("style", "display: none");
+                $(".pageNum1").attr("style", "");
+                var pages = $(".pageNum1");
+                console.log(pages);
+                $("#tableBody").append(pages);
+                if(pageNum > 1){
+                    $("#pageDirectory").attr("style", "display: initial");
+                    $("#pageButtons").html("");
+                    for(var j=1; j<=pageNum; j++){
+                        $("#pageButtons").append("<span><button class='btn btn info pageIndexBtn' data-page='"+j+"'>"+j+"</button></span>");
+                    };
+                };
             },
             error: function(errorObj){
                 console.log(errorObj);
             }
         });
     }
-    $("body").on("click", ".relatedSearchBtn", function(){
-        searchByArtist($(this).data("artist"));
+    $("body").on("click", ".relatedArtist", function(){
+        searchByArtist($(this).data("artist"), $("#startDateInput").val(), $("#endDateInput").val());
         
     }); 
     $(".relatedTab").click(function(){
@@ -169,11 +206,15 @@ jQuery(document).ready(function(){
         $(".inputBox").attr("required", "");
         $("#"+target+"Input").attr("required", "required");
     });
-    function findVenue(){
 
+    function findVenue(){
+        return;
     };
-    $("body").on("click", ".dropDownItem", function(){
-        searchByArtist($(this).data("artist"));
+    $(document).on("click", ".dropThis", function(){
+        debugger;
+        var bandName = $(this).data("artist");
+        console.log(bandName)
+        searchByArtist(bandName);
         $("#artistDropDown").attr("style", "display: none");
     });
     $("body").on("click", ".backToSearch", function(){
@@ -183,4 +224,27 @@ jQuery(document).ready(function(){
     $("body").on("click", ".buyTixBtn", function(){
         window.location.href = $(this).data("url");
     });
+    $("#lightSwitch").click(function(){
+        debugger;
+        if ($(this).attr("data-state") == "off"){
+            $(".wrapper").attr("style", "background-image: url('light.jpg')");
+            $(this).attr("data-state", "on");
+            return;
+        } else {
+            $(".wrapper").attr("style", "background-image: url('dark.png')");
+            $(this).attr("data-state", "off");
+        }
+    });
+    $("#spotifyConnect").click(function(){
+        window.location.href = $(this).data("url");
+    });
+    $("body").on("click", ".pageIndexBtn", function(){
+        $(".tourPage").attr("style", "display: none");
+        var page = $(".pageNum"+$(this).data("page"));
+        page.attr("style", "");
+        $("#tableBody").append(page);
+    });
 });
+
+     
+
